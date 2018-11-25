b96c979233b062a5b4b1f70a43ef32d9840c994c
Jedrzej <jbszczepaniak@gmail.com>
2018-11-25 12:58:33 +0100

    Test with race condition eliminated by sleep

--- healthcheck/healthchecker_test.go
+++ healthcheck/healthchecker_test.go
@@ -3,6 +3,7 @@ package healthcheck
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"testing"
 	"time"
 )
@@ -13,32 +14,59 @@ type MockSingleCheck struct {
 }
 
 func (mc *MockSingleCheck) SingleCheck() bool {
+	if mc.call >= len(mc.calls) {
+		panic(fmt.Sprintf("could not call mock for %d time, it has only %d values", mc.call+1, len(mc.calls)))
+	}
 	value := mc.calls[mc.call]
-	mc.call += 1
+	fmt.Println("now yielding: ", value)
+	mc.call++
 	return value
 }
 func TestHealthChecks(t *testing.T) {
-	hc := &HealthCheck{
-		PassedThreshold: 1,
-		FailedThreshold: 1,
-		Interval:        1 * time.Nanosecond, //does not matter
-		S:               &MockSingleCheck{calls: []bool{true}},
-		LogOutput:       bytes.NewBufferString(""),
+	testCases := []struct {
+		passedThreshold  int
+		failedThreshold  int
+		singleCheckCalls []bool
+		expectedHealth   []bool
+		description      string
+	}{
+		{
+			passedThreshold:  1,
+			failedThreshold:  1,
+			singleCheckCalls: []bool{true, false, true},
+			expectedHealth:   []bool{true, false, true},
+		},
 	}
-	c := make(chan time.Time)
-	after = func(d time.Duration) <-chan time.Time {
-		return c
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	hc.Run(ctx)
+	for _, tc := range testCases {
+		hc := &HealthCheck{
+			PassedThreshold: tc.passedThreshold,
+			FailedThreshold: tc.failedThreshold,
+			Interval:        0, // does not matter, we mock it
+			S:               &MockSingleCheck{calls: tc.singleCheckCalls},
+			LogOutput:       bytes.NewBuffer(nil),
+		}
 
-	if hc.IsHealthy() != false {
-		t.Fatal("expected to be unhealthy initially")
-	}
-	c <- time.Time{}
-	cancel()
+		c := make(chan time.Time)
+		// Mock `after` with `c` channel to make sure that
+		// test is time-independent
+		after = func(d time.Duration) <-chan time.Time {
+			return c
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		hc.Run(ctx)
 
-	if hc.IsHealthy() != true {
-		t.Fatal("expected to be healthy after one passed healthcheck")
+		if hc.IsHealthy() != false {
+			t.Fatal("expected to be unhealthy initially")
+		}
+
+		for i := 0; i < len(tc.singleCheckCalls); i++ {
+			c <- time.Time{}
+			time.Sleep(1 * time.Second)
+
+			if hc.IsHealthy() != tc.expectedHealth[i] {
+				t.Fatalf("run %d, expected healthy to be %t, but it wasn't", i, tc.expectedHealth[i])
+			}
+		}
+		cancel()
 	}
 }
